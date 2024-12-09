7f7d2891ddfbef65600cbd135cb2e7538ce05a74
Rodion Spiridonov <rodionspriridonov14@gmail.com>
2024-12-09 08:51:10 +0100

    Update build

--- src/components/errorMessage/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import img from "./error.gif";
-
-const ErrorMessage = () => {
-    return (
-        <img src={img} alt="Error" style={{ display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto' }} />
-    )
-}
-
-export default ErrorMessage;
--- src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import ComicsPage from "./ComicsPage";
-import MainPage from "./MainPage";
-import Page404 from "./404";
-import SingleComicPage from "./SingleComicPage";
-
-export {ComicsPage, MainPage, Page404, SingleComicPage};
