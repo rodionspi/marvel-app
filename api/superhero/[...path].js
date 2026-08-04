63828e82f984b8eaa8bec424e6cb2c794b9bd22c
Rodion <rodionspiridonov14@gmail.com>
2026-08-04 23:25:45 +0200

    feat: add superhero API endpoints for fetching by ID, pagination, and name search

---
+++ api/superhero/[...path].js
@@ -0,0 +1,27 @@
+const {
+    fetchAllHeroes,
+    findHeroById,
+    findHeroesPage,
+    findHeroByName
+} = require('../../lib/superhero');
+
+module.exports = async (req, res) => {
+    const path = req.query.path;
+    
+    if (!path || path.length === 0) {
+        return res.status(400).json({ error: 'Invalid path' });
+    }
+    
+    const [action, param] = path;
+    
+    switch (action) {
+        case 'page':
+            return res.json(await findHeroesPage(parseInt(param)));
+        case 'id':
+            return res.json(await findHeroById(param));
+        case 'search':
+            return res.json(await findHeroByName(param));
+        default:
+            return res.json(await findHeroById(action));
+    }
+};
